473ea3cb251c80fe402206ff29a10b998cc5d5eb
RIYASOLANKI03 <riyasolanki373@gmail.com>
2024-11-21 23:38:54 +0530

    Basic calculator

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+a = 20
+b = 30
+
+print("the value of" ,a, "+" ,3, "is", a+b)
+print("the value of" ,a, "*" ,3, "is", a*b)
+print("the value of" ,a, "-" ,3, "is", a-b)
+print("the value of" ,a, "/" ,3, "is", a/b)
