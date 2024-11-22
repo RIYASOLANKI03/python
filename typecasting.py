80774c220b8ab8f2e3ce0bfeb7ef0e5f87947dd8
RIYASOLANKI03 <riyasolanki373@gmail.com>
2024-11-23 00:36:55 +0530

    D-2

---
+++ typecasting.py
@@ -0,0 +1,6 @@
+name_of_the_user = input("enter your name") 
+length_of_name = len(name_of_the_user)
+
+print(type("number of letters in your name:"))#str
+print(type(length_of_name))#int
+print("number of letters in your name:" + str(length_of_name)) 
