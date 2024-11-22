9555c87787af62c049dc17334a1e57b36c56a635
RIYASOLANKI03 <riyasolanki373@gmail.com>
2024-11-23 00:01:23 +0530

    second day

---
+++ bandname generator.py
@@ -0,0 +1,4 @@
+print("wellcome to the name generator")
+city = input("which city did you grew in?|\n")
+pet = input("what is the name your pet?\n")
+print("your band name could be:" + city + " " + pet)
